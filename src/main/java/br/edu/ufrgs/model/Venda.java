bda5ea523b82e5e8b4d2e68f56c0caf46dfedd90
xaubet <jpoxaubet@gmail.com>
2026-04-24 20:40:59 -0300

    Adiciona o atributo vendaId à classe Venda

--- src/main/java/br/edu/ufrgs/model/Venda.java
+++ src/main/java/br/edu/ufrgs/model/Venda.java
@@ -5,10 +5,11 @@ import java.time.*;
 
 public class Venda{
     private Vendedor vendedor;
+    private int vendaId;
     private LocalDate dataVenda;
     private double valorVenda;
 
-    public Venda(Vendedor vendedor, LocalDate dataVenda, double valorVenda) {
+    public Venda(Vendedor vendedor, int vendaId, LocalDate dataVenda, double valorVenda) {
         this.vendedor = vendedor;
         this.dataVenda = dataVenda;
         this.valorVenda = valorVenda;
