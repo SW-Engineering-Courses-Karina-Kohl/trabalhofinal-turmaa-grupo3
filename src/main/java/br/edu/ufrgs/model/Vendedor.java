887176d6c3ead7ff258d06fcc1e3f91ec06f7525
ismael müller <ismoliveira@inf.ufrgs.br>
2026-04-24 22:51:20 -0300

    formatting fixes

--- src/main/java/br/edu/ufrgs/model/Vendedor.java
+++ src/main/java/br/edu/ufrgs/model/Vendedor.java
@@ -1,25 +1,24 @@
 package br.edu.ufrgs.model;
 
 /**
- * Classe que representa um vendedor.
- * Não há métodos setters, pois os atributos são definidos no construtor e não devem ser alterados posteriormente.
+ * classe que representa um vendedor.
+ * não há métodos setters, pois os atributos são definidos no construtor e não devem ser alterados posteriormente.
  */
 
-public class Vendedor{
-    private String nome;
-    private int vendedor_id;
+public class Vendedor {
+  private String nome;
+  private int vendedor_id;
 
-    public Vendedor(String nome, int vendedor_id) {
-        this.nome = nome;
-        this.vendedor_id = vendedor_id;
-    }
+  public Vendedor(String nome, int vendedor_id) {
+    this.nome = nome;
+    this.vendedor_id = vendedor_id;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public int getVendedor_id() {
-        return vendedor_id;
-    }
-
+  public int getVendedor_id() {
+    return vendedor_id;
+  }
 }
