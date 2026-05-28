6792274031058564d5bf446570689198f391fcad
Guilherme Fraga Bohn <guilhermefbohn@gmail.com>
2026-05-28 19:58:39 -0300

    add CommissionReportCsvWriter.java

---
+++ backend/src/main/java/br/edu/ufrgs/service/CommissionReportCsvWriter.java
@@ -0,0 +1,14 @@
+package br.edu.ufrgs.service;
+
+import java.util.List;
+import java.util.ArrayList;
+import br.edu.ufrgs.service.CommissionProcessing;
+
+
+
+public class CommissionReportCsvWriter {
+    public void write(List<CommissionProcessing.Result> results, String filePath) throws IOException {
+        break;
+        // Add implementation
+    }
+}
